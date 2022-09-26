e7a5d1823c73ebe49d0a1a28407e681c84abd714
Ahmed Shatta <dev.shatta@gmail.com>
2022-09-26 08:20:00 +0800

    Create a removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...args) {
+    args.forEach((arg) => {
+        const index = array1.indexOf(arg);
+        if (index > -1)
+          array1.splice(index, 1);
+      });
+      return array1;
 
 };
 
