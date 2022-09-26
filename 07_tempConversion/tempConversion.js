9d3ae4aee28301ded036aaad577612e86bed8b8c
Ahmed Shatta <dev.shatta@gmail.com>
2022-09-26 13:52:55 +0800

    Add compact form of the function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,12 +4,20 @@ const ftoc = function(temF) {
   return rTempC;
 };
 
+// const ftoc = function(f) {
+//   return Math.round((f - 32) * (5/9) * 10) / 10;
+//  };
+
 const ctof = function(temC) {
   let convTempF = temC * (9 / 5) + 32;
   let rTempF = Math.round(convTempF * 10) / 10;
   return rTempF;
 
 };
+
+// const ctof = function(c) {
+//   return Math.round(((c * 9/5) + 32) * 10) / 10;
+// };
 
 // Do not edit below this line
 module.exports = {
